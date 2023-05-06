7438e7cd647ac145dfd1b7dbed50807ac97b42ad
David Kamayor <davidkamayor@gmail.com>
2023-05-06 18:24:00 -0400

    Added console report for missing weather

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //API key is 1c0c421516ee4b6b865211829230605
 
 const input = {
-  getWeather: function () {
+  getLocation: function () {
     const userLocation = document.getElementById("location_input").value;
     console.log("User location: " + userLocation);
     return userLocation;
@@ -10,4 +10,9 @@ const input = {
 
 const locationSubmit = document.getElementById("location_submit");
 
-locationSubmit.addEventListener("input", input.getWeather);
+locationSubmit.addEventListener("click", function () {
+  const userLocation = input.getLocation();
+  if (!userLocation) {
+    console.log("No location submitted!");
+  }
+});
