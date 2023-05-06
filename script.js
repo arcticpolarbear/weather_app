30e514c72460beb2507609214a541205e7ae350e
David Kamayor <davidkamayor@gmail.com>
2023-05-06 18:10:31 -0400

    Fix locationSubmit callback

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //API key is 1c0c421516ee4b6b865211829230605
 
 const input = {
-  getWeather: function (textInput) {
-    const userLocation = document.getElementById("location_input");
+  getWeather: function () {
+    const userLocation = document.getElementById("location_input").value;
     console.log("User location: " + userLocation);
     return userLocation;
   },
@@ -10,4 +10,4 @@ const input = {
 
 const locationSubmit = document.getElementById("location_submit");
 
-locationSubmit.addEventListener("input", input.getWeather());
+locationSubmit.addEventListener("input", input.getWeather);
