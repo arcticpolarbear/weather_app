e2765561334bcff3ded74e104d8bc6bcf7daeacb
David Kamayor <davidkamayor@gmail.com>
2023-05-07 13:54:49 -0400

    Convereted inputHandling.submitLocation to try/catch

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
 const inputHandling = {
-  submit: function (location) {
+  submitLocation: async function (location) {
+    if (!location) {
+      console.log("User location not found");
+      return;
+    }
+    console.log("User location: " + location);
     //I know hardcoding API keys is bad, but this is an educational assignment
-    const apiKey = "1c0c421516ee4b6b865211829230605";
-    const userLocation = location;
-    fetch(
-      "https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${userLocation}`"
-    );
+    try {
+      const apiKey = "1c0c421516ee4b6b865211829230605";
+      const userLocation = location;
+      const response = await fetch(
+        `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${userLocation}`
+      );
+      const data = await response.json();
+      console.log("Response from server\n" + data);
+      return data;
+    } catch (e) {
+      console.log("Failed to return API call: " + e);
+    }
   },
 };
 
-const userLocation = document.getElementById("location_input").value;
-const locationSubmit = document.getElementById("location_submit");
+const submitButton = document.getElementById("location_submit");
 
-locationSubmit.addEventListener("click", function () {
-  const userLocation = inputHandling.getLocation();
-  if (!userLocation) {
-    console.log("No location submitted!");
-  }
+submitButton.addEventListener("click", function () {
+  const locationField = document.getElementById("location_input").value;
+  inputHandling.submitLocation(locationField);
 });
