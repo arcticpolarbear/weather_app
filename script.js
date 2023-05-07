4911153eaf88291bff441190a63d870e731d90cb
David Kamayor <davidkamayor@gmail.com>
2023-05-07 13:19:47 -0400

    Remove unneccesary function from inputHandling object

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
-//API key is 1c0c421516ee4b6b865211829230605
-
 const inputHandling = {
-  getLocation: function () {
-    const userLocation = document.getElementById("location_input").value;
-    console.log("User location: " + userLocation);
-    return userLocation;
-  },
   submit: function (location) {
     //I know hardcoding API keys is bad, but this is an educational assignment
     const apiKey = "1c0c421516ee4b6b865211829230605";
@@ -16,6 +9,7 @@ const inputHandling = {
   },
 };
 
+const userLocation = document.getElementById("location_input").value;
 const locationSubmit = document.getElementById("location_submit");
 
 locationSubmit.addEventListener("click", function () {
